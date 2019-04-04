19692f46818d256e3959d8b0cb1d8d03a5a6fba0
Jay-Goss <42222447+Jay-Goss@users.noreply.github.com>
2019-04-04 19:30:24 -0400

    Update arp.py

--- arp.py
+++ arp.py
@@ -6,10 +6,19 @@ def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
-    answered_list, unanswered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
-    print ("IP\t\t\tMAC Address\n-----------------------------------------")
+    clients_list = []
     for element in answered_list:
-        print(element[1].psrc + "\t\t" + element[1].hwsrc)
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        clients_list.append(client_dict)
+    return clients_list
 
-scan("10.0.2.1/24")
+def print_result(results_list):
+    print("IP\t\t\tMAC Address\n-----------------------------------------")
+    for client in results_list:
+        print(client["ip"] + "\t\t" + client["mac"])
+
+
+scan_result = scan("10.0.2.1/24")
+print_result(scan_result)
