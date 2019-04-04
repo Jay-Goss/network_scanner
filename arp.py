313447b6597535880e0b95a479055f8ac16e7460
Jay-Goss <42222447+Jay-Goss@users.noreply.github.com>
2019-04-04 15:16:39 -0400

    Update arp.py

--- arp.py
+++ arp.py
@@ -3,6 +3,13 @@
 import scapy.all as scapy
 
 def scan(ip):
-    scapy.arping(ip)
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered_list, unanswered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)
 
-scan("10.0.2.1")
+    print ("IP\t\t\tMAC Address\n-----------------------------------------")
+    for element in answered_list:
+        print(element[1].psrc + "\t\t" + element[1].hwsrc)
+
+scan("10.0.2.1/24")
